e702685d70c8ed3c346e604b1c11f149c890b0d0
otenkiwork <otenkiwork@gmail.com>
2024-02-18 12:00:39 +0900

    powershell 標準出力 リダイレクト サンプル更新

--- powershell/sample/smpl109_リダイレクト/smpl109_01.ps1
+++ powershell/sample/smpl109_リダイレクト/smpl109_01.ps1
@@ -1,5 +1,15 @@
 #
-# ・関数の戻り値は標準出力に出力される
+# ・Powershell の標準出力は、WindowsのコマンドプロンプトやLinuxのシェルとは異なる
+# ・WindowsのコマンドプロンプトやLinuxのシェルは、標準出力、エラー出力とコンソール出力は同一
+# ・Powershell は標準出力、エラー出力に相当するストリームに出力
+#     標準出力  ：Standard output stream(1>)
+#     エラー出力：Error output stream(2>)
+#     コマンドがパイプラインの最後のコマンドの場合、コンソールに出力される
+#
+# ・Write-Host　：コンソールへの直接出力（パイプラインには出力されない）
+# ・Write-Output：Standard output stream(1>)へ出力
+#
+# ・関数の戻り値はStandard output stream(1>)に出力される
 # ・return に指定した以外の関数内でパイプ出力したものも戻り値に含まれる
 #
 
@@ -10,7 +20,7 @@ $START_DIR_PATH = Split-Path $MyInvocation.MyCommand.Path -Parent
 $outFilePath = Join-Path $START_DIR_PATH "test.txt"
 
 function Func001() {
-    # 標準出力
+    # コンソールへ直接出力
     Write-Host "Func001_Write-Host"
 
     # パイプ（戻り値になる）
