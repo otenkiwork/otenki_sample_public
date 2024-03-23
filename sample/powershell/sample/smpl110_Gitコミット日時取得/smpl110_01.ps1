778baca9e426293400f1127d947cc9a01267ea65
otenkiwork <otenkiwork@gmail.com>
2024-03-23 17:19:49 +0900

    git コミット日時取得

--- sample/powershell/sample/smpl110_Gitコミット日時取得/smpl110_01.ps1
+++ sample/powershell/sample/smpl110_Gitコミット日時取得/smpl110_01.ps1
@@ -8,6 +8,7 @@ $dirpath = Join-Path $rootPath $gitDirName
 
 $fileList = Get-ChildItem $dirPath -File
 
+# 作業ディレクトリ内ファイル一覧取得して、最新のコミット日時を取得
 foreach ($file in $fileList) {
     $gitFilePath = Join-Path $gitDirName $file.Name
     $timeStamp = (git log -1 --date=format:"%Y/%m/%d %H:%M:%S" --pretty=format:"%ad" $gitFilePath)
@@ -18,3 +19,29 @@ foreach ($file in $fileList) {
         Write-Host $gitFilePath $timeStamp
     }
 }
+
+# 【タグ指定】作業ディレクトリ内ファイル一覧取得して、指定タグ以前で最新のコミット日時を取得
+foreach ($file in $fileList) {
+    $gitFilePath = Join-Path $gitDirName $file.Name
+    $timeStamp = (git log -1 --date=format:"%Y/%m/%d %H:%M:%S" --pretty=format:"%ad" v1.0.0 $gitFilePath)
+    if ($null -eq $timeStamp){
+        Write-Host $gitFilePath "Git未登録"
+    }
+    else {
+        Write-Host $gitFilePath $timeStamp
+    }
+}
+
+git config --local core.quotepath false
+$fileList = (git ls-files)
+foreach ($file in $fileList) {
+    $gitFilePath = $file
+    $timeStamp = (git log -1 --date=format:"%Y/%m/%d %H:%M:%S" --pretty=format:"%ad" v1.0.0 $gitFilePath)
+    if ($null -eq $timeStamp){
+        Write-Host $gitFilePath "Git未登録"
+    }
+    else {
+        Write-Host $gitFilePath $timeStamp
+    }
+}
+
